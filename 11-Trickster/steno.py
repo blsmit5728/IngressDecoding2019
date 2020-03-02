a913d834adf05229bd1bdb56f08894e0243df424
Brandon Smith <blsmit5728@gmail.com>
2020-03-02 06:19:45 -0500

    Adding Patron

---
+++ 11-Trickster/steno.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import sys
+
+steno = Image.open(sys.argv[1])
+decoded_w, decoded_l = steno.size
+decodedImg = Image.new('RGB',(decoded_w,decoded_l), 'grey')
+
+for pixel_x in range(0, decoded_w):
+    for pixel_y in range(0, decoded_l):
+        r,g,b = steno.getpixel((pixel_x, pixel_y))
+        if (r % 2):
+            # odd
+            decodedImg.putpixel((pixel_x,pixel_y), (255,255,255))
+        else:
+            decodedImg.putpixel((pixel_x,pixel_y), (0,0,0))
+        
+decodedImg.save('decoded.png')      
